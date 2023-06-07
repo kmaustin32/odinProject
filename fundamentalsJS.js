278ac3c6fa04080cf151001178defe4680d6aa19
Keenan (Ezra) Austin <reaper31292@gmail.com>
2023-06-07 20:13:21 +0300

    initial commit

---
+++ fundamentalsJS.js
@@ -0,0 +1,11 @@
+function testFunction() {
+  console.log("this is a test!")
+}
+
+testFunction();
+
+const another = (var1) => {
+  return var1 + " boogabooga"
+}
+
+console.log(another('chalupe'));
