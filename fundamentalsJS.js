a6e32e4d24966f01fd9dee680b6702e5f6287fa5
Keenan (Ezra) Austin <reaper31292@gmail.com>
2023-07-16 12:37:57 +0300

    added test function

--- fundamentalsJS.js
+++ fundamentalsJS.js
@@ -9,3 +9,26 @@ const another = (var1) => {
 }
 
 console.log(another('chalupe'));
+
+const hello = () => {
+  return "Hello, World!"
+}
+
+var username = "Farilla";
+
+console.log(hello());
+// console.log(username);
+function checkName(user) {
+  if (!user) return "Challabunga";
+
+  return user.toString();
+}
+
+function spellMe(str) {
+  for (let i=0; i<str.length; i++) {
+    console.log(str[i])
+  }
+}
+
+console.log(checkName(username));
+spellMe('Keenan is Ezra and they are a banana');
