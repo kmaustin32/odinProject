de5d4e407424fb81e736a459b0c1fb9e7c7dc209
Keenan Ezra Austin <ezra@LAPTOP-MGFQM7S5.>
2023-09-05 16:53:10 +0300

    Ternary Operator

--- fundamentalsJS.js
+++ fundamentalsJS.js
@@ -30,5 +30,23 @@ function spellMe(str) {
   }
 }
 
+const singleLine = (str1) => { console.log(str1 + " from singleLine") };
+singleLine(username);
+
 console.log(checkName(username));
 spellMe('Keenan is Ezra and they are a banana');
+
+var sides = 3
+var triangle = sides = 3 ? true : false;
+
+console.log(triangle);
+
+const cycle = (str3) => {
+  arr1 = []
+  for (let i=0; i<str3.length; i++) {
+    str3[i] == "e" ? arr1.push("EELZ") : arr1.push("Swolo");
+  }
+  return arr1
+}
+
+console.log(cycle("keenan"));
