5d25b66fa00f9b50a4dc7d83db775ab5369e7276
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2023-12-31 19:23:06 +0200

    Complete JS exercises 7 and 8

--- javascript/07_tempConversion/tempConversion.js
+++ javascript/07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let celsius = (5/9) * (fah -32);
+  return Math.round(celsius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahrenheit = ((9/5) * cel) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
