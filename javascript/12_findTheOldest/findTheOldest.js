cf3c03772fa0cf4b727c1281397de3bf3ddd3b7e
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2024-01-03 14:59:41 +0200

    Complete JS exercise 12

--- javascript/12_findTheOldest/findTheOldest.js
+++ javascript/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let current, oldest = people[0];
+  for (let person of people) {
+    current = person;
+    let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
+    if (currentAge > oldestAge) oldest = current;
+  }
+  return oldest.name;
+};
+
+function getAge(birth, death) { 
+  if (!death) {
+    return (new Date().getFullYear()) - birth;
+  } else {
+    return death - birth;
+  };
 };
 
 // Do not edit below this line
