05e2d9c200531781e3bbcf62fb0d64fd20e07d18
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2024-01-14 19:06:42 +0200

    Make shopping list with event listeners and delete buttons

---
+++ javascript/DOM Exercises/shoppingList/listScript.js
@@ -0,0 +1,36 @@
+const input = document.querySelector('input');
+const shoppingList = document.querySelector('ul');
+const addButton = document.querySelector('#addButton');
+
+addButton.addEventListener('click', () => {
+  addItemToList();
+})
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode == '13') {
+    addItemToList();
+  };
+  return;
+})
+
+function addItemToList() {
+  let itemName = input.value;
+  input.value = '';
+
+  let newLi = document.createElement('li');
+  let newSpan = document.createElement('span');
+  let newButton = document.createElement('button');
+
+  newSpan.textContent = itemName;
+  newButton.textContent = 'Delete';
+  
+  newLi.appendChild(newSpan);
+  newLi.appendChild(newButton);
+
+  shoppingList.appendChild(newLi);
+
+  newButton.addEventListener('click', () => {
+    shoppingList.removeChild(newLi);
+    input.focus();
+  });
+};
